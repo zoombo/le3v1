d77462b0fb5c0ad44978aebaba30e6174e7d69d2
Dmitry <AntiduPb@yandex.ru>
2017-06-18 12:36:38 +0300

    base experiments for ncurs

--- main.c
+++ main.c
@@ -1,9 +1,51 @@
-#include <stdio.h>
-#include <stddef.h>
+#include <ncurses.h>
+#include <stdbool.h>
+
+#define MAX_ITEMS 4
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
-	
-	return 0;
+
+	const char *marr[MAX_ITEMS] = {"Item1", "Item2", "Item3", "Item4"};
+	unsigned int updwn = 0;
+	unsigned int position = 0;
+
+	initscr();
+	keypad(stdscr, true);
+	while (true)
+	{
+		updwn = getch();
+		clear();
+		if (updwn == KEY_UP && position > 0)
+		{
+			position--;
+			//printw("UP\n");
+		}
+		if (updwn == KEY_DOWN && position < MAX_ITEMS - 1)
+		{
+			position++;
+			//printw("DOWN\n");
+		}
+		if (updwn == 'q')
+			break;
+
+
+		for (int i = 0; i < MAX_ITEMS; i++)
+		{
+			if (i == position)
+				printw("> %s\n", marr[i]);
+			else
+				printw("  %s\n", marr[i]);
+		}
+		//refresh();
+
+
+	}
+
+
+	printw("Hello!\n");
+	refresh();
+	getch();
+	endwin();
+
 }
