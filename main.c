4dbf9e7c0518a6911eaafcdcd79678cee2c14fee
Dmitry <AntiduPb@yandex.ru>
2017-06-23 23:12:11 +0300

    Create normal cursor rolling

--- main.c
+++ main.c
@@ -16,6 +16,7 @@ int main(int argc, char **argv) {
     unsigned int pressed_key = 0;
     // Текущая позиция "курсора" в списке.
     unsigned int position = 0;
+    unsigned int real_position = 0;
 
     dirslist_t *mdirs;
 
@@ -39,16 +40,38 @@ int main(int argc, char **argv) {
     mdirs = dirs_list(".");
 
     while (true) {
-
-        if (pressed_key == KEY_UP && position > 0)
-            position--;
-        if (pressed_key == KEY_DOWN && position < mdirs->count - 1)
-            position++;
+        /*
+         * Тут странная логика которая сделана для правильного 
+         * перемещения курсора по списку внутри окна.
+         * real_position - позиция указывающая на элемент списка.
+         * position - позиция списка выводимого в окно.
+         * BOTTOM_BORDER - ширина рамки.
+         * Положение position зависит от real_position так:
+         *   если real_position становится больше position, значит курсор
+         *   уходит за нижний край, значит увеличиваем position;
+         *   
+         *   если real_position становится меньше position, значит курсор
+         *   уходит за верхний край, значит уменьшаем position.
+         */
+        if (pressed_key == KEY_UP) {
+            if (real_position > 0)
+                real_position--;
+            if (real_position < (position + BOTTOM_BORDER) - getmaxy(mwin))
+                if (position > 0)
+                    position--;
+        }
+        if (pressed_key == KEY_DOWN) {
+            if (position < mdirs->count - 1) {
+                real_position++;
+                if (real_position > position)
+                    position++;
+            }
+        }
 
         // Если нажат ENTER
         if (pressed_key == '\n') {
             // Переходим в каталог на котором нажали ENTER
-            chdir(*(mdirs->list + position));
+            chdir(*(mdirs->list + real_position));
             // Освобождаем память занятую предыдущим списком.
             dirs_list_free(mdirs);
             // Освобождаем память переменной.
@@ -57,6 +80,7 @@ int main(int argc, char **argv) {
             mdirs = dirs_list(".");
             // Обнуляем позицию.
             position = 0;
+            real_position = 0;
             // Обнуляем чтобы не зациклилось.
             pressed_key = ' ';
             continue;
@@ -65,7 +89,7 @@ int main(int argc, char **argv) {
         // Выход по нажатию клавиши 'q'
         if (pressed_key == 'q')
             break;
-        
+
         // Отображает текущий каталог в заголовке.
         // Valgrind говорит что в этой функции иногда происходят ошибки.
         printw("\n > %s\n", get_current_dir_name());
@@ -73,6 +97,7 @@ int main(int argc, char **argv) {
         // Переменная хранящая позицию с которой 
         // начинать выводить список файлов.
         int tmp_pos = 0;
+        //if (real_position)
         // Вычисляем номер элемента с которого выводить список если
         // курсор ушел за нижний край.
         if (position + BOTTOM_BORDER > getmaxy(mwin)) {
@@ -82,7 +107,7 @@ int main(int argc, char **argv) {
 
         // Вывод списка.
         for (; tmp_pos < mdirs->count; tmp_pos++) {
-            if (tmp_pos == position) {
+            if (tmp_pos == real_position) {
                 attron(COLOR_PAIR(1));
                 printw("    %s\n", *(mdirs->list + tmp_pos));
                 attroff(COLOR_PAIR(1));
