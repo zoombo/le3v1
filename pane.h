19b35ce47ca530b06d7bbe2206fcccb183113806
Dmitry <AntiduPb@yandex.ru>
2017-06-30 15:14:29 +0300

    Add pane structure

--- pane.h
+++ pane.h
@@ -1,12 +1,28 @@
+#pragma once
+
+// Структура описывающая окно-панель со списком файлов.
+
 struct pane {
-	WINDOW *pane_window;
-	
-	dirlist_t *dirlist;
-	unsigned int position;
-	unsigned int real_position;
-	
-	char *current_directory;
-	
-	_Bool is_active;
-	
-}
+    // Указатель на хендл окна.
+    WINDOW *pane_window;
+    dirlist_t *dirlist;
+    // Позиция списка в окне.
+    unsigned int position;
+    // Позиция выделения окне.
+    unsigned int real_position;
+    
+    // Полный путь до текущего каталога.
+    char *current_directory;
+    // Указывает на то что окно активно в данный момент.
+    _Bool is_active;
+    
+    // Позиция окна по X.
+    unsigned int x_position;
+    // Позиция окна по Y.
+    unsigned int y_position;
+    // Размер окна по X.
+    unsigned int x_len;
+    // Размер окна по Y.
+    unsigned int y_len;
+    // Что еще?..
+};
